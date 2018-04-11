d407239bb9cbd1e4ae1b2750c1363418b5de9f61
Rohan Menon <rohan@Rohans-MacBook-Pro.local>
2018-04-10 19:18:58 -0500

    basic fs functionality

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-let [node, file, user, name, city] = process.argv;
+var fs = require("fs");
+
+
+var tempArr= ["Rohan", "Housotn", "4/10/18"];
+
+
+fs.appendFile('./log.txt', tempArr+"\n", function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+
+
+  fs.readFile('./log.txt', "utf-8", function(err, data){
+    if (err) throw err;
+    console.log(data);
+  });
+  
