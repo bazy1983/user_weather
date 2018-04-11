36bb3abaf7018363fcc0c6fbc9caf8653b282dde
Rovch <wesleyjacksonjr33@hotmail.com>
2018-04-10 19:15:41 -0500

    added your stuff

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-let [node, file, user, name, city] = process.argv;
+var inquirer = require("inquirer");
+var request = require("request");
+var weather = require('weather-js');
+
+inquirer.prompt([
+  {
+    name: "user",
+    type: "input",
+    message: "Enter your name!",
+  },
+  {
+    name: "name",
+    type: "input",
+    message: "Enter your gender!"
+  },
+  {
+    name: "city",
+    type: "input",
+    message: "Enter your strength!"
+  }
+]).then(function (answer) {
+console.log(answer);
+});
+
+
+
+
+function searcher() {
+
+var search = {
+  search: 'San Francisco, CA',
+  degreeType: 'F'
+}
+  weather.find(search, function (err, result) {
+    if (err) console.log(err);
+    console.log(JSON.stringify(result, null, 2));
+  });
+}
--- user.js
+++ user.js
@@ -1,9 +1,9 @@
-module.exports = function (){
-    this.name = name;
-    this.isAdmin = false;
-    this.checkAdmin = function (user){
-        if (user = "Admin"){
-            this.isAdmin = true;
-        }
-    }
-}
+// module.exports = function (){
+//     this.name = name;
+//     this.isAdmin = false;
+//     this.checkAdmin = function (user){
+//         if (user = "Admin"){
+//             this.isAdmin = true;
+//         }
+//     }
+// }
