b554fe893cff29529bce1ff7d72f1d6bd7070098
Mohammed Hameed <mohammed.hameed@email.com>
2018-04-10 19:08:22 -0500

    built user constructor

---
+++ user.js
@@ -0,0 +1,9 @@
+module.exports = function (){
+    this.name = name;
+    this.isAdmin = false;
+    this.checkAdmin = function (user){
+        if (user = "Admin"){
+            this.isAdmin = true;
+        }
+    }
+}
