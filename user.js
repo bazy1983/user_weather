3d112ed0c0381813f5811fe837d8f70d5a0be906
Rohan Menon <rohan@Rohans-MacBook-Pro.local>
2018-04-10 19:50:39 -0500

    made admin.js exports constructors

--- user.js
+++ user.js
@@ -1,38 +1,29 @@
-module.exports = function (){
-    this.name = name;
-    this.isAdmin = false;
-    this.checkAdmin = function (user){
-        if (user = "Admin"){
-            this.isAdmin = true;
-        }
-    }
-}
-function UserSearch(){
-    this.name = name;
-    this.location= location;
-    this.date = Date.now();
-    this.weatherSearch = function(){
-        
+module.exports =  function() {
+        this.name = name;
+        this.location = location;
+        this.date = Date.now();
+        this.weatherSearch = function () {
+    
             var search = {
-              search: 'San Francisco, CA',
-              degreeType: 'F'
+                search: 'San Francisco, CA',
+                degreeType: 'F'
             }
-              weather.find(search, function (err, result) {
+            weather.find(search, function (err, result) {
                 if (err) console.log(err);
                 console.log(JSON.stringify(result, null, 2));
-              });
+            });
+        }
+        this.checkAdmin = function (user) {
+            if (user = "Admin") {
+                this.isAdmin = true;
             }
-            
-}
-
-
-
-function AdminSearch(){
-    this.newUserSearch = function(){
-
+        }
+    
     }
 
-    this.getData = function(){
 
-    }
-}
+
+
+
+
+
